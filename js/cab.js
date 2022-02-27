5ef9581ed79d341371398fe452943c967e4a61ee
devulapallisai <ee20btech11013@iith.ac.in>
2022-02-27 16:36:00 +0530

    added

---
+++ js/cab.js
@@ -0,0 +1,10 @@
+let form = document.getElementById("formhere");
+let list = document.getElementById("itemshere");
+function cabform() {
+  form.style.display = "block";
+  list.style.display = "none";
+}
+function listhere() {
+  form.style.display = "none";
+  list.style.display = "block";
+}
